8291768ae87bd805b673e32be6713d6b93414545
Greg-GJE <gjegreg@gmail.com>
2023-05-23 00:53:20 +0530

    fixed bad indentation issue

--- exporters.py
+++ exporters.py
@@ -1,5 +1,6 @@
 from colorama import Fore, Style
 
+
 class DataExporter:
     def __init__(self, analyzer) -> None:
         self._analyzer = analyzer
@@ -16,17 +17,17 @@ class ConsoleExporter(DataExporter):
 
         print("\nOverview of each column\n")
 
-        print(  Fore.YELLOW + '|--------------------------------|----------------------|---------' +
-                '---|------------|------------|------------|------------|-----------------|---' +
-                '--------------|')
+        print(Fore.YELLOW + '|--------------------------------|----------------------|---------' +
+              '---|------------|------------|------------|------------|-----------------|---' +
+              '--------------|')
 
-        print(  Fore.YELLOW + f'| {"Column":30} | {"Standard Deviation":20} |' +
-                f' {"Max":10} | {"Min":10} |' +
-                f' {"Mean":10} | {"Median":10} | {"Mode":10} |'
-                f' {"Mode Frequency":15} | {"Unique Count":15} |')
+        print(Fore.YELLOW + f'| {"Column":30} | {"Standard Deviation":20} |' +
+              f' {"Max":10} | {"Min":10} |' +
+              f' {"Mean":10} | {"Median":10} | {"Mode":10} |'
+              f' {"Mode Frequency":15} | {"Unique Count":15} |')
 
-        print(  Fore.YELLOW + '|--------------------------------|----------------------|---'
-                + '---------|------'
+        print(Fore.YELLOW + '|--------------------------------|----------------------|---'
+              + '---------|------'
                 + '------|------------|------------|------------|-----------------|-------'
                 + '----------|')
 
@@ -40,17 +41,17 @@ class ConsoleExporter(DataExporter):
             mode_freq_val = self._analyzer.get_mode_frequency(header)
             unique_val = self._analyzer.get_unique_count(header)
 
-            print(  f'{Fore.YELLOW}| {Style.RESET_ALL}{header:30} {Fore.YELLOW}|'
-                    + f' {Style.RESET_ALL}{standard_deviation:<20.2f} {Fore.YELLOW}|'
+            print(f'{Fore.YELLOW}| {Style.RESET_ALL}{header:30} {Fore.YELLOW}|'
+                  + f' {Style.RESET_ALL}{standard_deviation:<20.2f} {Fore.YELLOW}|'
                     + f' {Style.RESET_ALL}{max_val:<10} {Fore.YELLOW}|'
                     + f' {Style.RESET_ALL}{min_val:<10}'
                     + f' {Fore.YELLOW}| {Style.RESET_ALL}{mean_val:<10.2f}'
                     + f' {Fore.YELLOW}| {Style.RESET_ALL}{median_val:<10} {Fore.YELLOW}|'
                     + f' {Style.RESET_ALL}{mode_val:<10} {Fore.YELLOW}|'
                     + f' {Style.RESET_ALL}{mode_freq_val:<15} {Fore.YELLOW}|'
-                    + f' {Style.RESET_ALL}{unique_val:<15} {Fore.YELLOW}|' )
-            print(  Fore.YELLOW + '|--------------------------------|--------------------'
-                    + '--|------------|------------|-----------'
+                    + f' {Style.RESET_ALL}{unique_val:<15} {Fore.YELLOW}|')
+            print(Fore.YELLOW + '|--------------------------------|--------------------'
+                  + '--|------------|------------|-----------'
                     + '-|------------|------------|-----------------|-----------------|')
 
         print('\nTop Correlations')
@@ -73,19 +74,19 @@ class FileExporter(DataExporter):
 
             fp.write("\nOverview of each column\n\n")
 
-            fp.write( '|--------------------------------|----------------------|---------' +
-                    '---|------------|------------|------------|------------|-----------------|---' +
-                    '--------------|\n')
+            fp.write('|--------------------------------|----------------------|---------' +
+                     '---|------------|------------|------------|------------|-----------------|---' +
+                     '--------------|\n')
 
-            fp.write( f'| {"Column":30} | {"Standard Deviation":20} |' +
-                    f' {"Max":10} | {"Min":10} |' +
-                    f' {"Mean":10} | {"Median":10} | {"Mode":10} |'
-                    f' {"Mode Frequency":15} | {"Unique Count":15} |\n')
+            fp.write(f'| {"Column":30} | {"Standard Deviation":20} |' +
+                     f' {"Max":10} | {"Min":10} |' +
+                     f' {"Mean":10} | {"Median":10} | {"Mode":10} |'
+                     f' {"Mode Frequency":15} | {"Unique Count":15} |\n')
 
-            fp.write( '|--------------------------------|----------------------|---'
-                    + '---------|------'
-                    + '------|------------|------------|------------|-----------------|-------'
-                    + '----------|\n')
+            fp.write('|--------------------------------|----------------------|---'
+                     + '---------|------'
+                     + '------|------------|------------|------------|-----------------|-------'
+                     + '----------|\n')
 
             for header in self._analyzer.headers:
                 standard_deviation = self._analyzer.get_std(header)
@@ -97,18 +98,18 @@ class FileExporter(DataExporter):
                 mode_freq_val = self._analyzer.get_mode_frequency(header)
                 unique_val = self._analyzer.get_unique_count(header)
 
-                fp.write(  f'| {header:30} |'
-                        + f' {standard_deviation:<20.2f} |'
-                        + f' {max_val:<10} |'
-                        + f' {min_val:<10}'
-                        + f' | {mean_val:<10.2f}'
-                        + f' | {median_val:<10} |'
-                        + f' {mode_val:<10} |'
-                        + f' {mode_freq_val:<15} |'
-                        + f' {unique_val:<15} |\n' )
-                fp.write( '|--------------------------------|--------------------'
-                        + '--|------------|------------|-----------'
-                        + '-|------------|------------|-----------------|-----------------|\n')
+                fp.write(f'| {header:30} |'
+                         + f' {standard_deviation:<20.2f} |'
+                           + f' {max_val:<10} |'
+                           + f' {min_val:<10}'
+                           + f' | {mean_val:<10.2f}'
+                           + f' | {median_val:<10} |'
+                           + f' {mode_val:<10} |'
+                           + f' {mode_freq_val:<15} |'
+                           + f' {unique_val:<15} |\n')
+                fp.write('|--------------------------------|--------------------'
+                         + '--|------------|------------|-----------'
+                         + '-|------------|------------|-----------------|-----------------|\n')
 
             fp.write('\nTop Correlations\n')
             fp.write("-------------------\n\n")
@@ -118,9 +119,9 @@ class FileExporter(DataExporter):
             for col_pairs, value in correlation_results.items():
                 (column1, column2) = col_pairs
                 if value > cur_val:
-                        cur_val = value
-                        top_pair = col_pairs
+                    cur_val = value
+                    top_pair = col_pairs
                 fp.write(f'{column1:20}{column2:20}{value}\n')
             if top_pair is not None:
-                fp.write(f"\nThere is a strong similarity between {top_pair[0]} and {top_pair[1]}")
-
+                fp.write(
+                    f"\nThere is a strong similarity between {top_pair[0]} and {top_pair[1]}")
