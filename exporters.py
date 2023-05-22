4dc9ba931992926daea57824be29cb3aed35d064
Greg-GJE <gjegreg@gmail.com>
2023-05-23 00:00:23 +0530

    added functionality of file export

--- exporters.py
+++ exporters.py
@@ -68,4 +68,51 @@ class FileExporter(DataExporter):
 
     def export(self):
         with open(self._out, 'w', encoding='utf-8') as fp:
-            pass
+            fp.write(f'Total entries: {self._analyzer.count}\n')
+            fp.write(f'Total Columns: {self._analyzer.total_headers}\n')
+
+            fp.write("\nOverview of each column\n\n")
+
+            fp.write( '|--------------------------------|----------------------|---------' +
+                    '---|------------|------------|------------|------------|-----------------|---' +
+                    '--------------|\n')
+
+            fp.write( f'| {"Column":30} | {"Standard Deviation":20} |' +
+                    f' {"Max":10} | {"Min":10} |' +
+                    f' {"Mean":10} | {"Median":10} | {"Mode":10} |'
+                    f' {"Mode Frequency":15} | {"Unique Count":15} |\n')
+
+            fp.write( '|--------------------------------|----------------------|---'
+                    + '---------|------'
+                    + '------|------------|------------|------------|-----------------|-------'
+                    + '----------|\n')
+
+            for header in self._analyzer.headers:
+                standard_deviation = self._analyzer.get_std(header)
+                max_val = self._analyzer.get_max(header)
+                min_val = self._analyzer.get_min(header)
+                mean_val = self._analyzer.get_mean(header)
+                median_val = self._analyzer.get_median(header)
+                mode_val = self._analyzer.get_mode(header)
+                mode_freq_val = self._analyzer.get_mode_frequency(header)
+                unique_val = self._analyzer.get_unique_count(header)
+
+                fp.write(  f'| {header:30} |'
+                        + f' {standard_deviation:<20.2f} |'
+                        + f' {max_val:<10} |'
+                        + f' {min_val:<10}'
+                        + f' | {mean_val:<10.2f}'
+                        + f' | {median_val:<10} |'
+                        + f' {mode_val:<10} |'
+                        + f' {mode_freq_val:<15} |'
+                        + f' {unique_val:<15} |\n' )
+                fp.write( '|--------------------------------|--------------------'
+                        + '--|------------|------------|-----------'
+                        + '-|------------|------------|-----------------|-----------------|\n')
+
+            fp.write('\nTop Correlations\n')
+            fp.write("-------------------\n\n")
+            correlation_results = self._analyzer.top_correlations
+            for col_pairs, value in correlation_results.items():
+                (column1, column2) = col_pairs
+                fp.write(f'{column1:20}{column2:20}{value}\n')
--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import sys
 import constants
 from importers import DataImporter
 from analyzers import Analyzer
-from exporters import ConsoleExporter
+from exporters import ConsoleExporter, FileExporter
 
 
 colorama.init(autoreset=True)
@@ -24,3 +24,14 @@ if __name__ == '__main__':
         console.export()
 
         print()
+        choice = input('Do you want to export the data (Y/N)?')
+
+        # if the choice is either y or yes
+        if choice.lower() in ['y', 'yes']:
+            output_file_loc = input('Enter the location of the output file: ')
+            file_exporter = FileExporter(dataset_analyzer, output_file_loc)
+            file_exporter.export()
+            print(f'Successfully exported to file {output_file_loc}')
+
+        print('\nThanks for trying out the application.')
+
