f1af6db8bb3feedda4198a21c8b3d1dc41149d42
Greg-GJE <gjegreg@gmail.com>
2023-05-22 23:40:47 +0530

    restructured the code and added basic functionality of getting the dataset from the user

--- main.py
+++ main.py
@@ -9,16 +9,25 @@ colorama.init(autoreset=True)
 class DataImporter:
 
     def __init__(self, file_location):
+        self._file = file_location
         self._dataframe = None
-        self._import_status = self._import(file_location)
+        self._import_status = self._import()
 
-    def _import(self, file_location):
+    def _import(self):
         try:
-            self._dataframe = pd.read_csv(file_location).dropna()
+            self._dataframe = pd.read_csv(self._file).dropna()
             return constants.IMPORTED
         except FileNotFoundError:
             print("The requested file could not be found!")
             return constants.NOT_FOUND
+
+    @property
+    def file(self):
+        return self._file
+
+    @property
+    def import_status(self):
+        return self._import_status
 
     @property
     def data(self):
@@ -26,104 +35,184 @@ class DataImporter:
 
 
 class Analyzer:
-    def __init__(self, importer) -> None:
+    def __init__(self, importer, num_correlations = 5) -> None:
         self._data = importer.data
         self._headers = self._get_headers()
-        self._mean = None
-        self._median = None
-        self._mode = None
-        self._correlation = None
+        self._top_correlations = self._get_top_correlations(num_correlations)
+
+    def _get_headers(self):
+        return self._data.columns
+
+    def _get_redundant_pairs(self):
+        '''Get diagonal and lower triangular pairs of correlation matrix'''
+        pairs_to_drop = set()
+        for i in range(0, self._data.shape[1]):
+            for j in range(0, i+1):
+                pairs_to_drop.add((self._headers[i], self._headers[j]))
+        return pairs_to_drop
+
+    def _get_top_correlations(self, n: int):
+        unstacked_corr = self._data.apply(lambda x: x.factorize()[0]).corr().abs().unstack()
+        labels_to_drop = self._get_redundant_pairs()
+        unstacked_corr = unstacked_corr.drop(labels=labels_to_drop).sort_values(ascending=False)
+        return unstacked_corr[:n]
 
     @property
     def headers(self):
         return self._headers
 
     @property
+    def top_correlations(self):
+        return self._top_correlations
+
+    @property
+    def count(self):
+        return len(self._data)
+
+    @property
+    def total_headers(self):
+        return len(self._headers)
+
+    @property
     def data(self):
         return self._data
 
-    def _get_headers(self):
-        return self._data.columns
+    def get_mean(self, col: str):
+        if col in self._headers and np.issubdtype(self._data[col].dtype, np.number):
+            return self._data[col].mean()
+        return '-'
 
-    def analyze(self):
+    def get_std(self, col: str):
+        if col in self._headers and np.issubdtype(self._data[col].dtype, np.number):
+            return self._data[col].std()
+        return '-'
+
+    def get_min(self, col: str):
+        if col in self._headers and np.issubdtype(self._data[col].dtype, np.number):
+            return self._data[col].min()
+        return '-'
+
+    def get_max(self, col: str):
+        if col in self._headers and np.issubdtype(self._data[col].dtype, np.number):
+            return self._data[col].max()
+        return '-'
+
+    def get_median(self, col: str):
+        if col in self._headers and np.issubdtype(self._data[col].dtype, np.number):
+            return self._data[col].median()
+        return '-'
+
+    def get_unique_count(self, col: str):
+        if col in self._headers:
+            return len(self._data[col].unique())
+        return '-'
+
+    def get_mode(self, col: str):
+        if col in self._headers:
+            return list(self._data[col].mode())[0]
+        return '-'
+
+    def get_mode_frequency(self, col: str):
+        if col in self._headers:
+            mode = self.get_mode(col)
+            return self._data[col].value_counts()[mode]
+        return '-'
+
+
+
+class DataExporter:
+    def __init__(self, analyzer) -> None:
+        self._analyzer = analyzer
+
+    def export(self):
         pass
 
 
+class ConsoleExporter(DataExporter):
 
-df = pd.read_csv('./realest.csv')
-# print(df.columns)
+    def export(self):
+        print(f'Total entries: {self._analyzer.count}')
+        print(f'Total Columns: {self._analyzer.total_headers}')
 
-class DataExporter:
-    def __init__(self, data, output_file) -> None:
+        print("\nOverview of each column\n")
+
+        print(  Fore.YELLOW + '|--------------------------------|----------------------|---------' +
+                '---|------------|------------|------------|------------|-----------------|---' +
+                '--------------|')
+
+        print(  Fore.YELLOW + f'| {"Column":30} | {"Standard Deviation":20} |' +
+                ' {"Max":10} | {"Min":10} |' +
+                f' {"Mean":10} | {"Median":10} | {"Mode":10} |'
+                f' {"Mode Frequency":15} | {"Unique Count":15} |')
+
+        print(  Fore.YELLOW + '|--------------------------------|----------------------|---'
+                + '---------|------'
+                + '------|------------|------------|------------|-----------------|-------'
+                + '----------|')
+
+        for header in self._analyzer.headers:
+            standard_deviation = self._analyzer.get_std(header)
+            max_val = self._analyzer.get_max(header)
+            min_val = self._analyzer.get_min(header)
+            mean_val = self._analyzer.get_mean(header)
+            median_val = self._analyzer.get_median(header)
+            mode_val = self._analyzer.get_mode(header)
+            mode_freq_val = self._analyzer.get_mode_frequency(header)
+            unique_val = self._analyzer.get_unique_count(header)
+
+            print(  f'{Fore.YELLOW}| {Style.RESET_ALL}{header:30} {Fore.YELLOW}|'
+                    + f' {Style.RESET_ALL}{standard_deviation:<20.2f} {Fore.YELLOW}|'
+                    + f' {Style.RESET_ALL}{max_val:<10} {Fore.YELLOW}|'
+                    + f' {Style.RESET_ALL}{min_val:<10}'
+                    + f' {Fore.YELLOW}| {Style.RESET_ALL}{mean_val:<10.2f}'
+                    + f' {Fore.YELLOW}| {Style.RESET_ALL}{median_val:<10} {Fore.YELLOW}|'
+                    + f' {Style.RESET_ALL}{mode_val:<10} {Fore.YELLOW}|'
+                    + f' {Style.RESET_ALL}{mode_freq_val:<15} {Fore.YELLOW}|'
+                    + f' {Style.RESET_ALL}{unique_val:<15} {Fore.YELLOW}|' )
+            print(  Fore.YELLOW + '|--------------------------------|--------------------'
+                    + '--|------------|------------|-----------'
+                    + '-|------------|------------|-----------------|-----------------|')
+
+        print('\nTop Correlations')
+        print("-------------------\n")
+        correlation_results = self._analyzer.top_correlations
+        for col_pairs, value in correlation_results.items():
+            (column1, column2) = col_pairs
+            print(f'{column1:20}{column2:20}{value}')
+
+
+class FileExporter(DataExporter):
+    def __init__(self, analyzer, output_file) -> None:
+        super().__init__(analyzer)
         self._out = output_file
-        self._data = data
 
     def export(self):
         with open(self._out, 'w', encoding='utf-8') as fp:
             pass
 
 
+if __name__ == '__main__':
+    dataset_location = input('Enter the location of the dataset file: ')
+    importer = DataImporter(dataset_location)
+
+    if importer.import_status == constants.NOT_FOUND:
+        print('The requested file could not be found')
+    else:
+        dataset_analyzer = Analyzer(importer)
+        print(f'\nHere are the details for the file: {importer.file}\n')
+
+        console = ConsoleExporter(dataset_analyzer)
+        console.export()
+
+        print()
 
 
-print(f'Total entries: {len(df)}')
-print(f'Total Columns: {len(df.columns)}')
-
-print("\nOverview of each column\n")
-
-print(Fore.YELLOW + '|--------------------------------|----------------------|------------|------------|------------|------------|------------|-----------------|-----------------|')
-print(Fore.YELLOW + f'| {"Column":30} | {"Standard Deviation":20} | {"Max":10} | {"Min":10} | {"Mean":10} | {"Median":10} | {"Mode":10} | {"Mode Frequency":15} | {"Unique Count":15} |')
-print(Fore.YELLOW + '|--------------------------------|----------------------|------------|------------|------------|------------|------------|-----------------|-----------------|')
-
-for column in df.columns:
-    standard_deviation = '-'
-    max_val = '-'
-    min_val = '-'
-    mean_val = '-'
-    median_val = '-'
-    mode_val = '-'
-    mode_freq_val = '-'
-
-    unique_val = len(df[column].unique())
-
-    if (np.issubdtype(df[column].dtype, np.number)):
-        standard_deviation = df[column].std()
-        max_val = df[column].max()
-        min_val = df[column].min()
-        mean_val = df[column].mean()
-        median_val = df[column].median()
-
-    modes = list(df[column].mode())
-
-    if len(modes) > 0:
-        mode_val = modes[0]
-        mode_freq_val = df[column].value_counts()[mode_val]
 
 
-    print(f'{Fore.YELLOW}| {Style.RESET_ALL}{column:30} {Fore.YELLOW}| {Style.RESET_ALL}{standard_deviation:<20.2f} {Fore.YELLOW}| {Style.RESET_ALL}{max_val:<10} {Fore.YELLOW}| {Style.RESET_ALL}{min_val:<10} {Fore.YELLOW}| {Style.RESET_ALL}{mean_val:<10.2f} {Fore.YELLOW}| {Style.RESET_ALL}{median_val:<10} {Fore.YELLOW}| {Style.RESET_ALL}{mode_val:<10} {Fore.YELLOW}| {Style.RESET_ALL}{mode_freq_val:<15} {Fore.YELLOW}| {Style.RESET_ALL}{unique_val:<15} {Fore.YELLOW}|')
-    print(Fore.YELLOW + '|--------------------------------|----------------------|------------|------------|------------|------------|------------|-----------------|-----------------|')
 
 
-def get_redundant_pairs(df):
-    '''Get diagonal and lower triangular pairs of correlation matrix'''
-    pairs_to_drop = set()
-    cols = df.columns
-    for i in range(0, df.shape[1]):
-        for j in range(0, i+1):
-            pairs_to_drop.add((cols[i], cols[j]))
-    return pairs_to_drop
 
-def get_top_correlations(df, n=5):
-    au_corr = df.apply(lambda x: x.factorize()[0]).corr(method='pearson').abs().unstack()
-    labels_to_drop = get_redundant_pairs(df)
-    au_corr = au_corr.drop(labels=labels_to_drop).sort_values(ascending=False)
-    return au_corr[:10]
 
-print('\nTop Correlations\n')
-print("----------------")
-res = get_top_correlations(df)
-for key, value in res.items():
-    (column1, column2) = key
-    print(f'{column1:20}{column2:20}{value}')
 
 # with open('hello.txt', 'w') as op:
 
