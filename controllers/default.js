25fa02040662fb598ce712d8a2fa94eac9adaefb
Greg-GJE <gjegreg@gmail.com>
2023-05-23 10:00:34 +0530

    added dockerfile for deployment

--- controllers/default.js
+++ controllers/default.js
@@ -18,7 +18,7 @@ function socket() {
         // Spawn terminal
         client.tty = Pty.spawn('python3', ['run.py'], {
             name: 'xterm-color',
-            cols: 200,
+            cols: 400,
             rows: 60,
             cwd: process.env.PWD,
             env: process.env
---
+++ dockerfile
@@ -0,0 +1,9 @@
+FROM nikolaik/python-nodejs:latest
+
+WORKDIR /app
+COPY . /app
+
+RUN pip install -r requirements.txt
+RUN npm install
+CMD ["node", "index.js"]
+
